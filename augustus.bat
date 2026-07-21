2e43ebf24b840374b0b44b782e1a0f6bcb4639d5
VerLouF <92933576+Deathlksr@users.noreply.github.com>
2026-07-21 21:33:02 +0500

    da

---
+++ augustus.bat
@@ -0,0 +1,16 @@
+@echo off
+chcp 65001 > nul
+:: 65001 - UTF-8
+
+cd /d "%~dp0"
+call service.bat status_zapret
+call service.bat check_updates
+call service.bat load_game_filter
+echo:
+
+set "BIN=%~dp0bin\"
+set "LISTS=%~dp0lists\"
+cd /d %BIN%
+
+start "zapret: %~n0" /min "%BIN%winws.exe" --wf-tcp=1337-1338,%GameFilter% ^
+--filter-tcp=1337-1338 --ipset-exclude="%LISTS%ipset-exclude.txt" --dpi-desync-any-protocol=1 --dpi-desync-cutoff=n5 --dpi-desync=multisplit --dpi-desync-split-seqovl=582 --dpi-desync-split-pos=1 --dpi-desync-split-seqovl-pattern="%BIN%tls_clienthello_4pda_to.bin" --dpi-desync-repeats=1-5 --new ^
